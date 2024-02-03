e2c6d59efb750473cd969cfdfbe2c31d5693a618
gagocarrilloedgar <edgargc.upc@gmail.com>
2024-02-03 20:05:27 +0100

    chore: add tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,6 @@ import * as React from "react";
 import { roboto, theme } from "~/theme";
 import { createEmotionCache } from "~/utils/createEmotionCache";
 import { MyAppProps } from "./_app";
-import Script from "next/script";
 
 interface MyDocumentProps extends DocumentProps {
   emotionStyleTags: React.JSX.Element[];
@@ -28,27 +27,28 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
         <meta name="emotion-insertion-point" content="" />
 
         {emotionStyleTags}
+        {/* GOOGLE TAG MANAGER - SCRIPT */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_ID}');`
+          }}
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-        />
-        <Script
-          id="google-analytics-init"
-          strategy="afterInteractive"
+        {/* GOOGLE TAG MANAGER - NOSCRIPT */}
+        <noscript
           dangerouslySetInnerHTML={{
-            __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
-                    `
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GOOGLE_TAG_MANAGER_ID}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`
           }}
-        />
+        ></noscript>
       </body>
     </Html>
   );
